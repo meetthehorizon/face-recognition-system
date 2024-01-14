599ccb32b97390eedf90ae6803e8f5c3b34ce03a
meetthehorizon <kshitij.sharma.cse22@itbhu.ac.in>
2024-01-14 21:01:17 +0530

    created DigiFace Dataset Class

---
+++ src/data/data_loader.py
@@ -0,0 +1,53 @@
+import os
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+
+class DigiFace(Dataset):
+	def __init__(self, path='data/raw', transform=None):
+		"""DigiFace dataset.
+		
+		Parameters
+		----------
+		path : str
+			Path to images directory
+		transform : callable
+			Transformation to apply to image and mask
+		"""
+		self.path = path
+		self.transform = transform
+		self.identities = os.listdir(path)
+		self.samples = self._generate_samples()
+
+	def __getitem__(self, index):
+		"""Load image and mask at index."""
+		if index >= len(self) or index < 0:
+			raise IndexError('Index out of range')
+		
+		identity, image_path = self.samples[index]
+		image = Image.open(image_path).convert('RGB')
+
+		if self.transform:
+			image = self.transform(image)
+		
+		return image, int(identity)
+
+	def _generate_samples(self):
+		"""Generate sample from dataset."""
+		samples = []
+		for identity in self.identities:
+			identity_path = os.path.join(self.path, identity)
+			images_path = os.listdir(identity_path)
+
+			for image_path in images_path:
+				image_path = os.path.join(identity_path, image_path)
+				samples.append((identity, image_path))
+		
+		return samples
+	
+	def __len__(self):
+		"""Return length of dataset."""
+		return len(self.samples)
+	
+if __name__ == '__main__':
+	print('testing script')
