99793bc55a6d4aca4b69b608a535664c37088454
meetthehorizon <kshitij.sharma.cse22@itbhu.ac.in>
2024-01-26 00:57:46 +0530

    Tranformer At Night Implementated
    Co-authored-by: Deepaprakash K <averagestud@users.noreply.github.com>

--- src/models/partfVit.py
+++ src/models/partfVit.py
@@ -8,63 +8,8 @@ from random import randint
 
 from src.data.data_loader import DigiFace
 from src.models.resnet50 import ResNet50
-
-
-def extract_landmarks_from_image(batch, landmarks, patch_size):
-    """Extract landmarks from image.
-
-    Parameters
-    ----------
-    batch : torch.Tensor
-            Batch of images with shape (batch_size, channels, height, width)
-    landmarks : torch.Tensor
-            Batch of landmarks with flattened co-ordinates of shape (batch_size, 2 * num_landmarks)
-    patch_size : list
-            Size of patches to extract of form [height, width]
-
-    Returns
-    -------
-    patches : torch.Tensor
-            Batch of patchs with shaepe (batch_size, num_landmarks, channels, height, width))
-    """
-
-    num_landmarks = landmarks.shape[1] // 2
-    batch_size, channels, image_height, image_width = batch.shape
-    patch_height, patch_width = patch_size
-
-    landmarks = landmarks.view(
-        batch_size, num_landmarks, 2
-    )  # Reshaping landmarks to (batch_size, num_landmarks, 2)
-
-    patch_range = [
-        [-patch_size[0] / 2, patch_size[0] / 2],
-        [-patch_size[1] / 2, patch_size[1] / 2],
-    ]  # the x and y range of the patches
-
-    grid_x, grid_y = torch.meshgrid(
-        torch.arange(patch_range[0][0], patch_range[0][1]),
-        torch.arange(patch_range[1][0], patch_range[1][1]),
-        indexing="ij",
-    )  # generating the grid of x and y values shape: (patch_height, patch_width)
-
-    sampling_grid = torch.stack(
-        (-grid_y, grid_x), dim=-1
-    )  # shape: (patch_height, patch_width, 2)
-    list_patches = []
-
-    for i in range(num_landmarks):
-        land = landmarks[:, i, :]
-        patch_grid = (land[:, None, None, :] + sampling_grid[None, :, :, :]) / (
-            0.5
-            * torch.tensor([image_height, image_width], dtype=torch.float32)[
-                None, None, None, :
-            ]
-        ) - 1
-        single_landmark_patch = F.grid_sample(batch, patch_grid, align_corners=False)
-        list_patches.append(single_landmark_patch)
-
-    patches = torch.stack(list_patches, dim=1)
-    return patches
+from src.models.transformer import Transformer
+from src.utils.pytorch_utils import extract_landmarks_from_image
 
 
 class part_fVit_with_landmark(nn.Module):
@@ -78,6 +23,7 @@ class part_fVit_with_landmark(nn.Module):
         mlp_dim=2048,
         num_heads=11,
         num_layers=12,
+        dropout=0.0,
     ):
         super().__init__()
         self.num_landmarks = num_landmarks
@@ -88,6 +34,7 @@ class part_fVit_with_landmark(nn.Module):
         self.mlp_dim = mlp_dim
         self.num_heads = num_heads
         self.num_layers = num_layers
+        self.dropout = dropout
 
         self.backbone = ResNet50(
             img_channels=in_channels, num_classes=2 * num_landmarks
@@ -135,22 +82,6 @@ class part_fVit_with_landmark(nn.Module):
         z0 += self.pos_embedding
 
         return z0
-
-
-def display_images(tensor):
-    # Assuming tensor has dimensions [num, channels, height, width]
-    num_images, num_channels, height, width = tensor.size()
-
-    # Reshape the tensor to [num, height, width, channels]
-    tensor = tensor.permute(0, 2, 3, 1)
-
-    # Display each image in a subplot
-    for i in range(num_images):
-        plt.subplot(1, num_images, i + 1)
-        plt.imshow(tensor[i].cpu().numpy())  # Assuming the tensor is on the CPU
-        plt.axis("off")
-
-    plt.show()
 
 
 def main():
